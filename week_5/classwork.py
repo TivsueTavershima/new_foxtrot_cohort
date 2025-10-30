2900e74ef3bfbe632a679b88ecd3d559ec5ffd2a
Tivsue Tavershima <tivsuetavershima@gmail.com>
2025-10-30 20:53:25 +0100

    week_5

--- week_5/classwork.py
+++ week_5/classwork.py
@@ -1,46 +1,38 @@
-# class  Employee:
+class  Employee:
 
-#   age = 20
-#   role = "back-end_dev"
-#   salary = 100
-  
-#   def __init__(self,name ,age,role,salary):
-#     self.name =name
-#     self.age =age
-#     self.role =role
-#     self.salary =salary
+  def __init__(self,name,age,role,salary):
+   self.name = name
+   self.age =age
+   self.role =role
+   self.salary =salary
     
-#   def information(self):
-#     return f"the enployee name is {self.name} , his age {self.age} , his role is {self.role} and his salary is {self.salary}."
+  def information(self):
+    return f"the enployee name is {self.name} , his age {self.age} , his role is {self.role} and his salary is {self.salary}."
   
-#   taver = Employee()
+joe = Employee(name="Tavershima", age=30, role="backend-dav", salary=40000)
+print(joe.information())
   
-#   print(taver.information())
-  
-#   print(information)
-  
-  
-#   class person:
-#     def __init__(self):
-#       self.name = "joe"
+class person:
+    def __init__(self):
+      self.name = "joe"
       
       
-#   class employee(person):
-#     def __init__(self):
-#       super().__init__()
+class employee(person):
+    def __init__(self):
+      super().__init__()
       
-#   joe = employee()
-#   print(joe.name)
+joe = employee()
+print(joe.name)
   
-# class person:
-#   def __init(self,name,age gender):
-#     self.name = name
-#     self.age = age
-#     self.genger = gender
+class person:
+  def __init__(self,name,age, gender):
+   self.name = name
+   self.age = age
+   self.genger = gender
     
-# class Employee():
-#   def __init__(self, name, age, gender):
-#     super().__init__(name, age, gender)
+class Employee():
+  def __init__(self, name, age, gender):
+    super().__init__(name, age, gender)
     
-#   joe = Employee(name="joe saders", age=22, gender="male")
-#   print(joe.age)
+  joe = Employee(name="joe saders", age=22, gender="male")
+  print(joe.age)
